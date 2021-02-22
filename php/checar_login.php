ff459a485d702904ea0ab46ee3911290f46a7076
Lucas0Santana <lucas1.stoly@gmail.com>
2021-02-22 14:54:43 -0300

    Perfil login no BD

--- php/checar_login.php
+++ php/checar_login.php
@@ -1,11 +1,32 @@
 <?php
 
 session_start();
-if($_POST["login"]=="a@gmail.com" && $_POST["senha"]=="0"){
-    $_SESSION["logou"] = 1;
-    header("Location: ../index.php");
-}
-else{
-    $_SESSION["logou"] = 2;
-    header("Location: ../login_area.php");
-}
+
+    include("conexaoBD.php");
+    $login=$_POST["login"];
+    $senha=$_POST["senha"];
+    $sql = "SELECT `idPerfil`, `nome`, `e-mail`, `senha`, `adm` FROM `perfil` WHERE `e-mail`='$login' and `senha`='$senha'";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        $email = $row["e-mail"];
+        $senha = $row["senha"];
+        $adm = $row["adm"];
+
+        $_SESSION["logou"] = 1;
+        header("Location: ../index.php");
+
+    }else{
+        $_SESSION["logou"] = 2;
+        header("Location: ../login_area.php");
+    }
+
+/*
+    if($_POST["login"]==$email && $_POST["senha"]==$senha){
+        $_SESSION["logou"] = 1;
+        header("Location: ../index.php");
+    }
+    else{
+        $_SESSION["logou"] = 2;
+        //header("Location: ../login_area.php");
+    }
+*/
--- pizzaria (3).sql
+++ pizzaria (3).sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 19-Fev-2021 às 16:27
--- Versão do servidor: 5.7.31
--- versão do PHP: 7.3.21
+-- Tempo de geração: 22-Fev-2021 às 17:40
+-- Versão do servidor: 10.4.10-MariaDB
+-- versão do PHP: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -46,6 +47,29 @@ INSERT INTO `bebida` (`nome`, `preco`, `status`, `idBebida`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `perfil`
+--
+
+DROP TABLE IF EXISTS `perfil`;
+CREATE TABLE IF NOT EXISTS `perfil` (
+  `idPerfil` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(30) NOT NULL,
+  `e-mail` varchar(50) NOT NULL,
+  `senha` varchar(16) NOT NULL,
+  `adm` tinyint(1) NOT NULL,
+  PRIMARY KEY (`idPerfil`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `perfil`
+--
+
+INSERT INTO `perfil` (`idPerfil`, `nome`, `e-mail`, `senha`, `adm`) VALUES
+(1, 'lucas', 'lucas1.stoly@gmail.com', '123', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `sabor`
 --
 
@@ -58,7 +82,14 @@ CREATE TABLE IF NOT EXISTS `sabor` (
   `disponibilidade` varchar(15) NOT NULL,
   `precoAdd` double DEFAULT NULL,
   PRIMARY KEY (`idSabor`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `sabor`
+--
+
+INSERT INTO `sabor` (`idSabor`, `nome`, `descricao`, `status`, `disponibilidade`, `precoAdd`) VALUES
+(11, 'aaaaaaa', 'Mussarela, parmesÃ£o, manjericÃ£o e orÃ©gano.', 'on', ',108', 0);
 
 -- --------------------------------------------------------
 
@@ -74,7 +105,14 @@ CREATE TABLE IF NOT EXISTS `tamanho` (
   `status` enum('on','off') NOT NULL,
   `qtdeSabor` int(11) NOT NULL,
   PRIMARY KEY (`idPizza`)
-) ENGINE=MyISAM AUTO_INCREMENT=108 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=109 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `tamanho`
+--
+
+INSERT INTO `tamanho` (`idPizza`, `nome`, `preco`, `status`, `qtdeSabor`) VALUES
+(108, 'aaa', 1.12, 'on', 22);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
