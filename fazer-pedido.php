d5d469c895af637ac1f14bb78a95cffbe2e64f4b
Lucas0Santana <lucas1.stoly@gmail.com>
2021-03-23 23:39:29 -0300

    segunda parte do fazer pedido

--- fazer-pedido.php
+++ fazer-pedido.php
@@ -11,7 +11,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     <!----------------------------------------------------------------------------------------------------------------->
     <!-------------------------| CSS |------------------------->
-    <link rel="stylesheet" type="text/css" href="css/pedido.css">
+    <link rel="stylesheet" type="text/css" href="css/fazer-pedido.css">
     <!--------------------------------------------------------->
 
 </head>
@@ -28,8 +28,7 @@
         
         
             <div class="pizza">
-                <h1 class="titulo">Pizzas</h1>
-                <hr>
+                
                     <!--<h4>Pizzas</h4>-->
                 
                     <?php
@@ -62,11 +61,12 @@
             <div class='accordion' id='accordionExample'>
             <div class='accordion-item'>
               <h2 class='accordion-header' id='headingOne'>
-                <button class='accordion-button' type='button' data-bs-toggle='collapse' data-bs-target='#collapseOne' aria-expanded='true' aria-controls='collapseOne'>
-                  pizza
+                <button class='accordion-button collapsed' type='button' data-bs-toggle='collapse' data-bs-target='#collapseOne' aria-expanded='true' aria-controls='collapseOne'>
+                <h1 class='titulo'>Pizzas</h1>
+                <hr>
                 </button>
               </h2>
-              <div id='collapseOne' class='accordion-collapse collapse show' aria-labelledby='headingOne' data-bs-parent='#accordionExample'>
+              <div id='collapseOne' class='accordion-collapse collapse' aria-labelledby='headingOne' data-bs-parent='#accordionExample'>
                 <div class='accordion-body'>
 
 
@@ -93,21 +93,40 @@
                       echo"
                         <!------------------------------------------------------------------------------------------------->
                         <!-- Button trigger modal -->
-                      <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#exampleModal'>
+                      <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#e$idPizza[$qtdeTamanhoCad]'>
                       $pizza[$qtdeTamanhoCad]
                       </button>
                       <br>
                       <!-- Modal -->
-                      <div class='modal fade' id='exampleModal' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
+                      <div class='modal fade' id='e$idPizza[$qtdeTamanhoCad]' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
                         <div class='modal-dialog'>
                           <div class='modal-content'>
                             <div class='modal-header'>
-                              <h5 class='modal-title' id='exampleModalLabel'>Modal title</h5>
-                              <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
-                            </div>
-                            <div class='modal-body'>
-                            $pizza[$qtdeTamanhoCad]
-                            </div>
+                            <h5 class='modal-title' id='staticBackdropLabel'> Escolha até $qtdeSabor[$qtdeTamanhoCad] Sabores </h5>
+                            <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+                        </div>
+                        <div class='modal-body'>";
+                        for ($e=0; $e < $qtdeSabor[$qtdeTamanhoCad]; $e++) { 
+                            $numSabor = $e+1;
+                            echo" Sabor $numSabor:
+                            <select class='form-select'>
+                            <option selected disabled hidden>Clique Aqui Para Escolher Um Sabor</option>
+                            <option>Nenhum</option>";
+                            for ($j=0; $j < sizeof($disponibilidade); $j++) { 
+                                $dispo = explode(",", $disponibilidade[$j]);  
+                            
+                            for ($i=0; $i < sizeof($dispo); $i++) {   
+                                                                    
+                                if($idPizza[$qtdeTamanhoCad] == $dispo[$i]){
+
+                                    echo"<option> $sabor[$j] </option>";
+                                }}}            
+                            echo"
+                            </select><br>";
+                        }
+                        
+                        echo"<p class='saboresCarrinho'>*caso não queria a mesma quantidade de sabores do máximo, escolhar a opção nenhum </p>
+                        </div>
                             <div class='modal-footer'>
                               <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Close</button>
                               <button type='button' class='btn btn-primary'>Save changes</button>
@@ -127,12 +146,100 @@
                          
                             ?>
                             </div> <!-- Fim da div pizza -->
-                            </div> <!-- Fim da div geral -->
 
-                            </form>
+                  </div>
+                           
+              
+            <div class="bebida">
+            
+            <!--<button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#finalizar"> Finalizar Pedido </button>-->
+            <?php
+
+            echo"
+
+            <div class='accordion' id='accordionExample'>
+            <div class='accordion-item'>
+              <h2 class='accordion-header' id='headingOne'>
+                <button class='accordion-button collapsed' id='botao' type='button' data-bs-toggle='collapse' data-bs-target='#collapseTwo' aria-expanded='true' aria-controls='collapseTwo'>
+                <h1 class='titulo'>Bebidas</h1>
+                <hr>
+                </button>
+              </h2>
+              <div id='collapseTwo' class='accordion-collapse collapse' aria-labelledby='headingTwo' data-bs-parent='#accordionExample'>
+                <div class='accordion-body'>
+
+
+            ";
+
+                include "php\conexaoBD.php";
+
+                $sql = "SELECT * FROM `bebida` Where `status`='on' and `cardapio`='on' ORDER BY nome ASC";
+                $result = $conn->query($sql);   
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+                        $nome_bebida = $row["nome"];
+                        $preco = $row["preco"];
+                        $idBebida = $row["idBebida"];
+                        $cardapioBD = $row["cardapio"];
+                        $status = $row["status"];
+
+                        $formatter = new NumberFormatter('pt-BR', NumberFormatter:: CURRENCY);
+                        $brl = $formatter->formatCurrency($preco, 'BRL');
+
+                      echo"
+
+                      <!-- Button trigger modal -->
+                      <button type='button' class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#e$idBebida'>
+                      $nome_bebida
+                    </button>
+                      <br>
+                        <!-- Modal -->
+                        <div class='modal fade' id='e$idBebida' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
+                          <div class='modal-dialog'>
+                            <div class='modal-content'>
+                              <div class='modal-header'>
+                                <h5 class='modal-title' id='exampleModalLabel'>Modal title</h5>
+                                <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+                              </div>
+                              <div class='modal-body'>
+                              $nome_bebida
+                              </div>
+                              <div class='modal-footer'>
+                                <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Close</button>
+                                <button type='button' class='btn btn-primary'>Save changes</button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>  
+                        
+                        ";
+
+
+
+                    }
+                    echo"
+                    </div>
+                    </div>
+                    </div>
+                  </div>";
+                  }
+
+                  
+                  ?>
+
+
+
+</div> <!-- Fim da div geral -->
+
+
+</form>
+
+
+<button class="confirmar">Adicionar</button>
                             
-                            
-                            
+<hr>
+<h1>Itens Adicionados</h1>
+<hr>
                   
 
 
